ca50a50d15d8bf035582cb2774d9b4abad2d66a2
anniekellner <annie.kellner@colostate.edu>
2025-05-22 12:38:50 -0600

    deleted duplicate entries (bc camera name change) and joined all environmental data to camCoords DF

--- Code/Environmental_Covs/combine_envCovs_intoDF.R
+++ Code/Environmental_Covs/combine_envCovs_intoDF.R
@@ -18,7 +18,48 @@ camCoords <- readRDS("./Data/Spatial/camCoords.Rds")
 camCoords <- camCoords %>%
   select(`Camera Trap Name`:Assigned_Longitude)
 
-camCoords <- camCoords %>%
-  left_join(imgHab, by = "Camera Trap Name")
+## Find duplicates (renamed cameras)
 
-dups <- filter(camCoords, `Camera Trap Name` == "1994-N02A-01")
+dups <- camCoords %>%
+  group_by(`Camera Trap Name`) %>%
+  filter(n() > 1) 
+
+dupsDelete <- dups %>% # get dataframe of duplicates
+  ungroup() %>%
+  filter(row_number() %in% c(4,5,6)) # remove rows with old names/data
+
+camCoords_noDups <- anti_join(camCoords, dupsDelete) # seems to have worked
+
+
+##    ----    JOIN COVARIATE DATA   ----    ##
+
+# Join with raster habitat data
+
+camCoordsEnv <- camCoords_noDups %>%
+  left_join(imgHab)
+
+camCoordsEnv <- camCoordsEnv %>%
+  rename("Raster_Habitat_Class" = "Class_Name")
+
+
+# Join shapefile data
+
+shpHab <- shpHab %>% # rename columns so they match
+  rename(`Camera Trap Name` = "Camera.Trap.Name") %>%
+  rename("shp_Habitat_Class" = "cam_veg_type")
+
+camCoordsEnv <- camCoordsEnv %>%
+  left_join(shpHab)
+
+
+# Join Distance Data
+
+camCoordsEnv <- camCoordsEnv %>%
+  left_join(dist)
+
+
+# Save file
+
+saveRDS(camCoordsEnv, file = "./Data/Spatial/camCoords_withEnv.Rds")
+
+
