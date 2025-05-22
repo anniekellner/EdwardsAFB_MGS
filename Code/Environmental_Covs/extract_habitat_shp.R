e1f055179384afdaf8b19a8e199f04e235f0e69d
anniekellner <annie.kellner@colostate.edu>
2025-05-21 23:53:46 -0600

    extracted data; saving new df

--- Code/Environmental_Covs/extract_habitat_shp.R
+++ Code/Environmental_Covs/extract_habitat_shp.R
@@ -10,6 +10,8 @@ library(tidyverse)
 library(terra)
 library(sf)
 library(terra)
+library(tmap)
+library(tmaptools)
 
 ##  --  LOAD AND PREP DATA  --  ##
 
@@ -28,7 +30,39 @@ st_crs(hab) # in UTM 11N
 camCoords <- readRDS("./Data/Spatial/camCoords.Rds")
 
 # Make DF into spatial object
+
 camCoordsSF <- st_as_sf(camCoords, 
                         coords = c("Assigned_Longitude", "Assigned_Latitude"), 
                         crs = "EPSG:4326")
 
+# Reproject camera shp to UTM 11N
+
+camCoordsUTM <- st_transform(camCoordsSF, st_crs(hab))
+st_crs(camCoordsUTM) # looks good
+
+# Plot to be sure projections align - looks good
+
+tm_shape(hab) + 
+  tm_polygons(fill = "VEGETATION") + 
+  tm_shape(camCoordsUTM) + 
+  tm_symbols(fill = "black")
+
+# Create buffer around points
+
+camCoordsBuff <- st_buffer(camCoordsUTM, dist = 150)
+
+##  --  DATA EXTRACTION   --  ##
+
+hab_data <- st_intersection(camCoordsBuff, hab)
+
+# Calculate majority
+
+majority_hab <- hab_data %>%
+  group_by(Camera.Trap.Name) %>%
+  summarize(cam_veg_type = names(which.max(table(VEGETATION))),
+            .groups = "drop")
+
+# Save file
+
+majority_hab <- st_drop_geometry(majority_hab) # save as dataframe, not spatial object
+#saveRDS(majority_hab, file = "./Data/Spatial/Habitat/Derived/shp_habitat.Rds")
