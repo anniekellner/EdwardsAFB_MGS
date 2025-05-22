eb1bbc84e0a5f179a36626cd5e8a5d0bc8ea83b8
anniekellner <annie.kellner@colostate.edu>
2025-05-22 05:51:43 -0600

    need to check which camera for row 21 is correct

--- Code/Environmental_Covs/combine_envCovs_intoDF.R
+++ Code/Environmental_Covs/combine_envCovs_intoDF.R
@@ -2,3 +2,23 @@
 ###                   COMBINE ENVIRONMENTAL COVS INTO DF                     ###
 ################################################################################
 
+library(tidyverse)
+
+
+##    ----    LOAD DATA   ----    ##
+
+imgHab <- readRDS("./Data/Spatial/Habitat/Derived/img_habitat.Rds")
+
+shpHab <- readRDS("./Data/Spatial/Habitat/Derived/shp_habitat.Rds")
+
+dist <- readRDS("./Data/Spatial/Habitat/Derived/dist_to_water.Rds")
+
+camCoords <- readRDS("./Data/Spatial/camCoords.Rds")
+
+camCoords <- camCoords %>%
+  select(`Camera Trap Name`:Assigned_Longitude)
+
+camCoords <- camCoords %>%
+  left_join(imgHab, by = "Camera Trap Name")
+
+dups <- filter(camCoords, `Camera Trap Name` == "1994-N02A-01")
