2bb6a9c642afa70b60baebcc1071a183ed603e38
anniekellner <annie.kellner@colostate.edu>
2025-05-21 22:42:52 -0600

    this script is moot at this point bc the info is in the rmd file

--- Code/modeling_firstpass.R
+++ Code/modeling_firstpass.R
@@ -5,9 +5,25 @@
 library(unmarked)
 library(tidyverse)
 
+# Load data
+umf <- readRDS('./Data/umf_05152025.Rds')
+
+# Model with temporal covariates and random effects
+mgs.occu <- occu(~ scOrdinal + scOrdinal2 ~ 1 + (1|site),  # Detection ~ temporal covariates, Occupancy ~ random effect
+                 data = umf,
+                 linkPsi = "logit")
+
+# Model summary
+summary(mgs.occu)
+
+# Confidence intervals
+confint(mgs.occu, type = "det", method = "normal")
+confint(mgs.occu, type = "state", method = "normal")
+
+# Predictions
+pred_scaled <- predict(mgs.occu, type = "state")
 
 
-umf <- readRDS('./Data/umf_05142025.Rds')
 
-
-mgs.occu <- occu(~ordinal + ordinal2 ~(1|site))
+# Save model results
+saveRDS(mgs.occu, "./Results/mgs_occu_model.Rds")
