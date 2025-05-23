827a037dabfab1437701ea2211edb4b3f9f15252
anniekellner <annie.kellner@colostate.edu>
2025-05-22 22:38:14 -0600

    added environmental covs to RDS dataframe

--- Code/add_envCovs_createUMF.R
+++ Code/add_envCovs_createUMF.R
@@ -3,6 +3,33 @@
 ################################################################################
 
 library(tidyverse)
+library(unmarked)
+
 
 ##    ----    LOAD AND PREP DATA    ----    ##
+
+## Environmental Covs
+
+envCams <- readRDS("./Data/Spatial/camCoords_withEnv.Rds")
+
+envCams <- envCams %>%
+  select(`Camera Trap Name`, 
+         `Study Site`, 
+         Raster_Habitat_Class, 
+         shp_Habitat_Class,
+         Dist_to_Stream) %>%
+  rename("Camera_Name" = `Camera Trap Name`)
+
+## Detection Data
+
+detAll <- readRDS("./Data/Detection/Derived/detection_withNAs_05112024.Rds")
+
+# Join env data by camera name
+
+detEnv <- detAll %>%
+  left_join(envCams)
+
+
+##    ----    CREATE UMF    ----    ##
+
 
