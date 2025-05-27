52d127c41dfd8145f911ea8405bc6ec1c43a4f98
anniekellner <annie.kellner@colostate.edu>
2025-05-26 20:33:30 -0600

    Reclassified Raster Habitat as Desert Scrub v Other, JTree Woodland + Other

--- Code/Environmental_Covs/habitat_reclass.R
+++ Code/Environmental_Covs/habitat_reclass.R
@@ -7,7 +7,7 @@
   # Joshua Tree Woodland/Other
 
 
-# Reclassify habitat as Desert Scrub/Not Desert Scrub
+# Reclassify habitat as Desert Scrub/Other
 
 allHab <- readRDS("./Data/Spatial/camCoords_withEnv_05262025.Rds") 
 
@@ -21,6 +21,8 @@ table(hab$Raster_Habitat_Class)
 #saveRDS(hab, file = "./Data/Spatial/Habitat/DesertScrubOnly.Rds")
 
 
+# Reclassify Habitat as Joshua Tree Woodland/Other
+
 jtree <- allHab %>%
   mutate(Raster_Habitat_Class = ifelse(Raster_Habitat_Class == "Joshua Tree Woodland",
                                        "Joshua Tree Woodland",
@@ -28,3 +30,18 @@ jtree <- allHab %>%
 table(jtree$Raster_Habitat_Class)
 
 #saveRDS(jtree, file = "./Data/Spatial/Habitat/JTreeOnly.Rds")
+
+
+###
+
+# Join with independent cams only
+
+detHab <- det %>%
+  left_join(hab)
+
+#saveRDS(detHab, file = "./Data/Covariates/indCams_adultsOnly_DesertScrub.Rds")
+
+detJtree <- det %>%
+  left_join(jtree)
+
+#saveRDS(detJtree, file = "./Data/Covariates/indCams_adultsOnly_JTree.Rds")
