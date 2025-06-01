64bfc5e0ea5a5e2ad797f7f12c602d1b8ec22af8
anniekellner <annie.kellner@colostate.edu>
2025-06-01 17:02:25 -0600

    new color for streams. Need to make sure the cameras are correctly attributed to stream distance.

--- Code/plot_occupancy.R
+++ Code/plot_occupancy.R
@@ -2,20 +2,19 @@
 ###                 MAP OCCUPANCY PROBABILITY ACROSS SITES                   ###
 ################################################################################
 
+library(tidyverse)
 library(tmap)
 library(tmaptools)
 library(sf)
 library(viridisLite)
+library(cols4all)
 
 
 # Load data
 
 streams <- st_read("./Data/Spatial/Habitat/Water_features/WaterFeature_L.shp") # river shp
 
-indCamsSF <- st_read("./Results/indCameras_spatial.shp") # cameras shp (independent)
-
-camsDF <- readRDS("./Data/Spatial/Derived/independentCams_DF_dist2stream.Rds") # DF with new coords and dist vals
-
+indCams <- readRDS("./Data/Detection/Derived/Detection_NAs_indCams_adultsOnly.Rds")
 
 
 # Remove cameras without data (site IDs 53 and 107)
@@ -23,39 +22,33 @@ camsDF <- readRDS("./Data/Spatial/Derived/independentCams_DF_dist2stream.Rds") #
 indCams[53,] # AFRL-NW-05
 indCams[107,] # HQA-38-03
 
-indCamsSF <- indCamsSF %>%
-  filter(!(CamerNm == "AFRL-NW-05" | 
-              CamerNm == "HQA-38-03"))
+indCams <- indCams %>%
+  filter(!(Camera_Name == "AFRL-NW-05" | 
+              Camera_Name == "HQA-38-03"))
 
-camsDF <- camsDF %>%
-  filter(!(CameraName == "AFRL-NW-05" | 
-             CameraName == "HQA-38-03"))
 
-camsDF <- camsDF %>%
-  mutate(dist_numeric = as.numeric(Dist_to_Stream))
-
-all.equal(indCamsSF$Dst_t_S, camsDF$dist_numeric)
-occuProbs <- bind_cols(indCams, all_adults_pred_occu)
+camProbsSF <- bind_cols(indCamsSF, occuProbs) # occuProbs is from MGS_Final_Analysis.Rmd
 
 occuProbs <- occuProbs %>%
   select(Camera_Name, Assigned_Latitude, Assigned_Longitude, Dist_to_Stream, Predicted)
 
+
 # Make into spatial object
 
-occuProbsSF <- st_as_sf(occuProbs, 
-                        coords = c("Assigned_Longitude", "Assigned_Latitude"),
-                        crs = "EPSG:4326")
+#occuProbsSF <- st_as_sf(occuProbs, 
+                        #coords = c("Assigned_Longitude", "Assigned_Latitude"),
+                        #crs = "EPSG:4326")
 
-occuProbsUTM <- st_transform(occuProbsSF, st_crs(streams))
+#occuProbsUTM <- st_transform(occuProbsSF, st_crs(streams))
 
 tmap_mode('view')
 
 tm_shape(streams) + 
-  tm_lines(col = "blue") +
-tm_shape(occuProbsSF) + 
+  tm_lines(col = "#3D5B9F") +
+tm_shape(camProbsSF) + 
   tm_symbols(fill = "Predicted",
              size = 0.5,
-             popup.vars = "Dist_to_Stream",
+             popup.vars = "Dst_t_S",
              fill.scale = tm_scale_continuous(values = "viridis")) +
   tm_layout(legend.reverse = TRUE) 
  
