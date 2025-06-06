84765bde05a0ec974a11864b0d7ed8a48efd8038
anniekellner <annie.kellner@colostate.edu>
2025-06-06 06:14:42 -0600

    created df for dates centered around May 22

--- Code/Detection/adjust_center_date_for_det_trend.R
+++ Code/Detection/adjust_center_date_for_det_trend.R
@@ -8,3 +8,30 @@
   # a springtime waxing and waning presence is part of MGS life history
 
 
+library(tidyverse)
+
+
+##    ----    LOAD DATA   ----    ##
+
+df <- readRDS("~/Repos/MGS/Data/indCams_adults_envCovs_06022025.Rds")
+
+# Get dates from column names
+
+dates <- colnames(df)[2:111]
+dates <- mdy(dates)
+ord <- yday(dates)
+
+# Center dates around May 22 (ordinal date 143)
+target_center <- 143 
+scaled_dates <- ord - target_center
+
+# Create a data frame with the original and scaled dates
+date_df <- data.frame(
+  original_date = dates,
+  ordinal_date = ord,
+  scaled_date = scaled_dates
+)
+
+# Save the scaled dates for later use
+#saveRDS(date_df, "~/Repos/MGS/Data/Detection/datesDF.Rds")
+
