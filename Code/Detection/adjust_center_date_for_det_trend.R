37662a9989e375508f3682f63b507573db37e0eb
anniekellner <annie.kellner@colostate.edu>
2025-06-06 05:54:14 -0600

    initial commit

---
+++ Code/Detection/adjust_center_date_for_det_trend.R
@@ -0,0 +1,10 @@
+###############################################################################
+###       ADJUST ORDINAL DATE TO MATCH PARABOLIC DETECTION TREND            ###
+###############################################################################
+
+# Because the study start and end dates are somewhat arbitrary, we are adjusting
+  # the center of the date spread to capture the parabolic detection trend. 
+  # We know a priori (and based on visual inspection of our data) that 
+  # a springtime waxing and waning presence is part of MGS life history
+
+
