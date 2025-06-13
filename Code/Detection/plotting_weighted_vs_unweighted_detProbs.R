2f4ec72aee9ef028a4f65ef6e5dd4d24b8b6853f
anniekellner <annie.kellner@colostate.edu>
2025-06-12 23:34:24 -0600

    found out I can't weight the dates, so this is useless

---
+++ Code/Detection/plotting_weighted_vs_unweighted_detProbs.R
@@ -0,0 +1,34 @@
+################################################################################
+###           WEIGHTED VS UNWEIGHTED DETECTION PROBABILITIES                ###
+################################################################################
+
+m5 <- occu(~scOrdinal ~ Dist_Scaled,
+           data = umf) # From MGS_Final_Analysis.Rmd
+
+detProbs <- predict(m5, type = "det")
+
+
+##    ----    PLOTS   ----    ##
+
+
+# Unweighted, logit scale
+
+detProbDF <- data.frame(
+  "Date" = colnames(finalDF[,2:111]),
+  "Detection_Prob" = detProbs$Predicted
+)
+
+detProbDF$Date <- mdy(detProbDF$Date)
+
+ggplot(detProbDF, aes(x = Date, y = Detection_Prob)) + 
+  geom_point() + 
+  geom_line() + 
+  scale_x_date(date_breaks = "5 days",
+               date_labels = "%m/%d") +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x = "Date", y = "Detection Probability")
+
+
+# Weighted
+
